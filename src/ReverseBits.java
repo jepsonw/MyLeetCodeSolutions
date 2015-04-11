363949e9d69b276c5ff8b6f88efded5b0e469efa
kaijiezhou <kaijiezhou913@gmail.com>
2015-04-11 14:59:17 -0700

    finish ReverseBits and RotateArray

--- src/ReverseBits.java
+++ src/ReverseBits.java
@@ -4,15 +4,15 @@ public class ReverseBits {
         long unsigned= n&0x0ffffffff;
         int gate=1;
         int result=0;
-        for (int i=1;i<32;i++){
+        for (int i=0;i<32;i++){
             System.out.println(i+"::"+"result-> "+Integer.toBinaryString(result)+"	gate-> "+Integer.toBinaryString(gate)+"	add"+Long.toBinaryString(unsigned&gate)+"	"+Integer.toHexString(result));
-            result+=(unsigned&gate);
             result<<=1;
+            result+=((unsigned&gate)==0?0:1);
             gate<<=1;
         }
         return result;
     }
 	public static void main(String[]args){
-		new ReverseBits().reverseBits(1);
+		new ReverseBits().reverseBits(3);
 	}
 }
---
+++ src/RotateArray.java
@@ -0,0 +1,32 @@
+
+public class RotateArray {
+	public void rotate(int[] nums, int k) throws Exception{
+        int length=nums.length;
+        k=k%length;
+        revert(nums,0,length-1);
+        revert(nums,0,k-1);
+        revert(nums,k,length-1);
+        
+    }
+    
+    private void revert(int[]nums, int start, int end) throws Exception{
+        if(end-start>nums.length||end<start|| start<0||end>=nums.length)
+            throw new Exception("Invalid Boundary");
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            ++start;
+            --end;
+        }
+    }
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int[]nums={1,2,3,4,5};
+		new RotateArray().rotate(nums, 3);
+		for(int i:nums){
+			System.out.println(i+" ");
+		}
+	}
+
+}
