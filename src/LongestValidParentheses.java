0b8b4d69e17af64e83e0b78c8aaeb769d3f4c6ac
kaijiezhou <kaijiezhou913@gmail.com>
2015-07-07 18:48:03 +0800

    add three solutions

---
+++ src/LongestValidParentheses.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kaijiezhou on 7/2/15.
+ */
+public class LongestValidParentheses {
+}
---
+++ src/SearchForARange.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kaijiezhou on 7/3/15.
+ */
+public class SearchForARange {
+}
